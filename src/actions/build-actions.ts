9d0d6d318b9b73444800f71716a879c02492d212
Madvux <tom.wilkolazki@gmail.com>
2023-10-08 17:43:41 +0200

    worked on frontend side of sending data

--- src/actions/build-actions.ts
+++ src/actions/build-actions.ts
@@ -1,7 +1,8 @@
 "use server"
 
 import { prisma } from "@/db"
-import { Champion, Item, Prisma, Role, Rune } from '@prisma/client'
+import { Champion, Item, Role } from '@prisma/client'
+import { error } from "console"
 import { revalidatePath } from "next/cache"
 
 export const deleteBuild = async (id: string) => {
@@ -9,226 +10,85 @@ export const deleteBuild = async (id: string) => {
     try {
         await prisma.$connect()
         await prisma.build.delete({ where: { id } })
-
+        revalidatePath("/")
     } catch (error) {
         return {
-            error: "something went wrong"
+            error: "Some error occurred while deleting..., database is not responding"
         }
     } finally {
         await prisma.$disconnect()
     }
-
-    revalidatePath("/")
-    // }
-
-    // export const createBuild = async (championId: string, roleId: string, items: string[], runes: string) => {
-    //     let build: Prisma.BuildCreateInput
-
-    //     // build = {
-    //     //     champion: { connect: { id: championId } },
-    //     //     role: { connect: { id: roleId } },
-    //     //     items: { connect: items.map(e => ({ id: e })) },
-    //     //     runes: { connect: { id: runes } }
-
-    //     // }
-    //     // console.log(build)
-    //     // try {
-    //     //     await prisma.$connect
-    //     //     // await prisma.build.create({ data: { championId, roleId, items: {connect: items.map(e => ({id: e}))  }, runes:{connect: {id: runes}} }})
-    //     //     await prisma.build.create({include: })
-    //     // } catch (error) {
-    //     //     return {
-    //     //         error: "something went wrong"
-    //     //     }
-    //     // } finally {
-    //     //     await prisma.$disconnect
-
-    //     // }
-    //     try {
-    //         await prisma.$connect
-    //         // await prisma.build.create({ data: { championId, roleId, items: {connect: items.map(e => ({id: e}))  }, runes:{connect: {id: runes}} }})
-    //         await prisma.build.create({
-    //             data: {
-    //                 championId: championId,
-    //                 roleId: roleId,
-    //                 items: {
-    //                     connect:
-    //                     items.map(e => ({id: e}))
-    //                 },
-    //                 // runes: {
-    //                 //     connect: [{
-    //                 //         id: runes,
-    //                 //         perks: {
-    //                 //             some: {
-    //                 //                 id: { equals: undefined },
-    //                 //             }
-    //                 //         },
-    //                 //     }, {
-    //                 //         id: runes,
-    //                 //         perks: {
-    //                 //             some: {
-    //                 //                 id: {in: ["",""]},
-    //                 //             }
-    //                 //         },
-    //                 //     }
-    //                 //     ]
-    //                 // },
-    //                 // runes: {
-    //                 //     connect:[
-    //                 //         {id: ""},
-    //                 //         { id: ""},
-    //                 //     ]
-    //                 // }
-
-    //             }
-    //         })
-    //     } catch (error) {
-    //         return {
-    //             error: "something went wrong"
-    //         }
-    //     } finally {
-    //         await prisma.$disconnect
-
-    //     }
-    //     revalidatePath("/")
-    // }
 }
 
-export const createBuild = async () => {
+type FormData = {
+    roleID: string,
+    championID: string,
+    itemsIDArray: string[],
+    primaryPerksIDArray: string[]
+    secondaryPerksIDArray: string[]
+}
+export const createBuild = async ({roleID,championID,itemsIDArray,primaryPerksIDArray,secondaryPerksIDArray}:FormData) => {
 
-    const role: Role = {
-        id: 'fa8ab034-ef97-4554-be37-a495e85e9d89',
-        name: 'top'
+    if(primaryPerksIDArray.length != 4) return {error: "Pick 4 primary runes"}
+    if(secondaryPerksIDArray.length != 2) return {error: "Pick 2 secondary runes"}
+
+    try {
+        await prisma.$connect()
+
+        await prisma.$transaction(async (tx) => {
+            let createPrimaryRunes = await tx.runeBuild.create(
+                {
+                    data: {
+                        runes: {
+                            connect: primaryPerksIDArray.map(e => ({ id: e }))
+                        }
+                    }
+                }
+            )
+            let createSecondaryRunes = await tx.runeBuild.create(
+                {
+                    data: {
+                        runes: {
+                            connect: secondaryPerksIDArray.map(e => ({ id: e }))
+                        }
+                    }
+                }
+            )
+            let data = await tx.build.create({
+                include: {
+                    items: true,
+                    runes: true
+                },
+                data: {
+                    champion: {
+                        connect: {
+                            id: championID
+                        }
+                    },
+                    role: {
+                        connect: {
+                            id: roleID
+                        }
+                    },
+                    items: {
+                        connect: itemsIDArray.map(item => ({ id: item }))
+                    },
+                    runes: {
+                        connect: [{ id: createPrimaryRunes.id }, { id: createSecondaryRunes.id }]
+                    }
+                }
+            })
+            console.log(data)
+        })
+        revalidatePath("/")
     }
-    const champion: Champion = {
-        id: '0a9ca0be-a27a-4ec4-9861-cd2ade5d4c9e',
-        league_id: 266,
-        name: 'Aatrox'
+    catch (error) {
+        console.error(error)
+        return {
+            error: "Error while saving data try again later or contact administrator"
+        }
+    } finally {
+        await prisma.$disconnect()
+
     }
-    const items: Item[] = [
-        {
-            id: '1dd9fc52-72e7-4fc2-aabd-ad580faa7632',
-            league_id: 2010,
-            name: 'Total Biscuit of Everlasting Will',
-            buildId: null
-        },
-        {
-            id: 'bda0001b-f139-4ce9-ba4f-faa31edca2ec',
-            league_id: 2015,
-            name: 'Kircheis Shard',
-            buildId: null
-        }
-    ]
-
-
-    const primaryPerksArray = [
-        {
-            id: '615c79e7-89fc-4038-bd4f-39b41dedaa44',
-            league_id: 8214,
-            name: 'Summon Aery',
-            key: 'SummonAery',
-            row: 0,
-            runeId: 'f1cb1212-5b0f-4e27-98d5-19eaee4696c0'
-        },
-        {
-            id: '713a0f7c-5a87-48b8-aefb-96b39f64737e',
-            league_id: 8226,
-            name: 'Manaflow Band',
-            key: 'ManaflowBand',
-            row: 1,
-            runeId: 'f1cb1212-5b0f-4e27-98d5-19eaee4696c0'
-        },
-        {
-            id: 'e9649ed3-842f-4296-9d32-b81fd8e5ad69',
-            league_id: 8210,
-            name: 'Transcendence',
-            key: 'Transcendence',
-            row: 2,
-            runeId: 'f1cb1212-5b0f-4e27-98d5-19eaee4696c0'
-        },
-        {
-            id: '21fa54c5-785c-4502-b2f5-c747ff2140df',
-            league_id: 8237,
-            name: 'Scorch',
-            key: 'Scorch',
-            row: 3,
-            runeId: 'f1cb1212-5b0f-4e27-98d5-19eaee4696c0'
-        },
-
-    ]
-
-    const secondaryPerksArray = [
-        {
-            id: '9d78b11f-78ea-47c8-a675-e6f8a8e0b7b6',
-            league_id: 8304,
-            name: 'Magical Footwear',
-            key: 'MagicalFootwear',
-            row: 1,
-            runeId: '6a9e5baf-41fa-4bcc-9701-c853f6566928'
-        },
-
-        {
-            id: 'cbac2b94-d006-41c9-83f7-cd40923158fb',
-            league_id: 8316,
-            name: 'Minion Dematerializer',
-            key: 'MinionDematerializer',
-            row: 2,
-            runeId: '6a9e5baf-41fa-4bcc-9701-c853f6566928'
-        },
-        {
-            id: '5fc78d5f-0b15-40bf-98be-ac3a0f8c6e61',
-            league_id: 8410,
-            name: 'Approach Velocity',
-            key: 'ApproachVelocity',
-            row: 3,
-            runeId: '6a9e5baf-41fa-4bcc-9701-c853f6566928'
-        }
-        
-    ]
-
-    let createPrimaryRunes = await prisma.runeBuild.create(
-        {
-            data: {
-                runes: {
-                    connect: primaryPerksArray.map(e => ({ id: e.id }))
-                }
-            }
-        }
-    )
-    let createSecondaryRunes = await prisma.runeBuild.create(
-        {
-            data: {
-                runes: {
-                    connect: secondaryPerksArray.map(e => ({ id: e.id }))
-                }
-            }
-        }
-    )
-    let data = await prisma.build.create({
-        include: {
-            items: true,
-            runes: true
-        },
-        data: {
-            champion: {
-                connect: {
-                    id: champion.id
-                }
-            },
-            role: {
-                connect: {
-                    id: role.id
-                }
-            },
-            items: {
-                connect: items.map(item => ({ id: item.id }))
-            },
-            runes: {
-                connect: [{ id: createPrimaryRunes.id }, { id: createSecondaryRunes.id }]
-            }
-        }
-    })
-    console.log(data)
-
-    //allRunesWithPerksArray.map((rune, index) => ({ id: rune[index].id }))
 }
--- src/components/CreateBuildForm.tsx
+++ src/components/CreateBuildForm.tsx
@@ -15,163 +15,114 @@ type PageProps = {
     champions: Champion[],
     runes: RuneWithPerks[],
     items: Item[],
-    role: string
+    roleID: string
 }
-type PickedRunes = {
-        keystone: string
-        perk_0?: string,
-        perk_1?: string,
-        perk_2?: string,
-        perk_3?: string,
-}
-const CreateBuildForm = async ({ champions, runes, items, role }: PageProps) => {
+
+const CreateBuildForm = ({ champions, runes, items, roleID }: PageProps) => {
     const router = useRouter()
-    // const [picked, setPicked] = useState<PickedRunes>({keystone: runes?.at(0)})
+    const [data, setData] = useState({
+        championID: "",
+        primary_tree: "",
+        secondary_tree: "",
+        itemsIDArray: [],
+        primaryPerksIDArray: [],
+        secondaryPerksIDArray: [],
+    })
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState({
         className: "",
         msg: ""
     })
-const bui: Prisma.BuildCreateInput = {
-    role: {
-        create: undefined,
-        connectOrCreate: undefined,
-        connect: undefined
-    },
-    champion: {
-        create: undefined,
-        connectOrCreate: undefined,
-        connect: undefined
+
+    const primaryPerksIDArray = [
+        '615c79e7-89fc-4038-bd4f-39b41dedaa44',
+        '713a0f7c-5a87-48b8-aefb-96b39f64737e',
+        'e9649ed3-842f-4296-9d32-b81fd8e5ad69',
+        '21fa54c5-785c-4502-b2f5-c747ff2140df'
+    ]
+    const secondaryPerksIDArray = [
+        '9d78b11f-78ea-47c8-a675-e6f8a8e0b7b6',
+        'cbac2b94-d006-41c9-83f7-cd40923158fb',
+        '5fc78d5f-0b15-40bf-98be-ac3a0f8c6e61'
+    ]
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(data)
+
+        setLoading(true)
+        setError({
+            className: "alert alert-info",
+            msg: "Creating build..."
+        })
+        try {
+            const res = await createBuild({roleID,
+                championID: data.championID,
+                itemsIDArray: data.itemsIDArray,
+                primaryPerksIDArray: data.primaryPerksIDArray,
+                secondaryPerksIDArray: data.secondaryPerksIDArray
+            })
+            if (res?.error) {
+                setError({
+                    "className": "alert alert-error",
+                    "msg": res.error
+                })
+            } else {
+                setError({
+                    "className": "alert alert-success",
+                    "msg": "Created!"
+                })
+                
+                setTimeout(() => {
+                    router.prefetch('/')
+                    router.push('/')
+                }, 1000)
+            }
+        } catch (error) {
+            console.error(error)
+        }
+        setLoading(false)
+
     }
-}
-
-// need only id's
-// const role: Role = {
-//     id: 'fa8ab034-ef97-4554-be37-a495e85e9d89',
-//     name: 'top'
-// }
-// const champion: Champion = {
-//     id: '0a9ca0be-a27a-4ec4-9861-cd2ade5d4c9e',
-//     league_id: 266,
-//     name: 'Aatrox'
-// }
-// const items: Item[] = [
-//     {
-//         id: '1dd9fc52-72e7-4fc2-aabd-ad580faa7632',
-//         league_id: 2010,
-//         name: 'Total Biscuit of Everlasting Will',
-//         buildId: null
-//     },
-//     {
-//         id: 'bda0001b-f139-4ce9-ba4f-faa31edca2ec',
-//         league_id: 2015,
-//         name: 'Kircheis Shard',
-//         buildId: null
-//     }
-// ]
-// const primaryPerksArray = [
-//     {
-//         id: '615c79e7-89fc-4038-bd4f-39b41dedaa44',
-//         league_id: 8214,
-//         name: 'Summon Aery',
-//         key: 'SummonAery',
-//         row: 0,
-//         runeId: 'f1cb1212-5b0f-4e27-98d5-19eaee4696c0'
-//     },
-//     {
-//         id: '713a0f7c-5a87-48b8-aefb-96b39f64737e',
-//         league_id: 8226,
-//         name: 'Manaflow Band',
-//         key: 'ManaflowBand',
-//         row: 1,
-//         runeId: 'f1cb1212-5b0f-4e27-98d5-19eaee4696c0'
-//     },
-//     {
-//         id: 'e9649ed3-842f-4296-9d32-b81fd8e5ad69',
-//         league_id: 8210,
-//         name: 'Transcendence',
-//         key: 'Transcendence',
-//         row: 2,
-//         runeId: 'f1cb1212-5b0f-4e27-98d5-19eaee4696c0'
-//     },
-//     {
-//         id: '21fa54c5-785c-4502-b2f5-c747ff2140df',
-//         league_id: 8237,
-//         name: 'Scorch',
-//         key: 'Scorch',
-//         row: 3,
-//         runeId: 'f1cb1212-5b0f-4e27-98d5-19eaee4696c0'
-//     },
-
-// ]
-
-// const secondaryPerksArray = [
-//     {
-//         id: '9d78b11f-78ea-47c8-a675-e6f8a8e0b7b6',
-//         league_id: 8304,
-//         name: 'Magical Footwear',
-//         key: 'MagicalFootwear',
-//         row: 1,
-//         runeId: '6a9e5baf-41fa-4bcc-9701-c853f6566928'
-//     },
-
-//     {
-//         id: 'cbac2b94-d006-41c9-83f7-cd40923158fb',
-//         league_id: 8316,
-//         name: 'Minion Dematerializer',
-//         key: 'MinionDematerializer',
-//         row: 2,
-//         runeId: '6a9e5baf-41fa-4bcc-9701-c853f6566928'
-//     },
-//     {
-//         id: '5fc78d5f-0b15-40bf-98be-ac3a0f8c6e61',
-//         league_id: 8410,
-//         name: 'Approach Velocity',
-//         key: 'ApproachVelocity',
-//         row: 3,
-//         runeId: '6a9e5baf-41fa-4bcc-9701-c853f6566928'
-//     }
-    
-// ]
-    const handleSubmit = async (data: FormData) => {
-        // setLoading(true)
-        // setError({
-        //     className: "alert alert-info",
-        //     msg: "Creating build..."
-        // })
-        createBuild()
-        // console.log(picked)
-        // data.forEach((key, value) => console.log(key, value))
-        // const champion = data.get("champion") as string
-        // const runes = data.get("primary_rune") as string
-        // const perks = data.getAll("perks") as string[]
-        // const items = data.getAll("items") as string[]
-
-        // try {
-        //     const res = await createBuild(champion, role, items, runes)
-        //     if (res?.error) {
-        //         setError({
-        //             "className": "alert alert-error",
-        //             "msg": res.error
-        //         })
-        //     }
-        // } catch (error) {
-        //     console.error(error)
-        // } finally {
-        //     setLoading(false)
-        // }
-        // router.prefetch('/')
-        // router.push('/')
+    function handleChange(e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value
+        })
+        console.log(data)
+    }
+    function handleChangeCheckbox(e: ChangeEvent<HTMLInputElement>) {
+        let newArray = [...data.itemsIDArray, e.target.value];
+        // @ts-ignore never type
+        if (data.itemsIDArray.includes(e.target.value)) {
+            newArray = newArray.filter(x => x !== e.target.value)
+        }
+        setData({ ...data, [e.target.name]: newArray })
+        console.table(data.itemsIDArray)
     }
 
-    // function handleChange(e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) {
-    //     setPicked({
-    //         ...picked,
-    //         [e.target.name]: e.target.value
-    //     })
-    //     console.log(picked)
-    // }
-
+    //works for checkboxes but no radios (picks too many)
+    function handleChangeRadioPrimary(e: ChangeEvent<HTMLInputElement>) {
+        //max 4 picked
+        let newArray = [...data.primaryPerksIDArray, e.target.value];
+        // @ts-ignore never type
+        if(data.primaryPerksIDArray.includes(e.target.value)){
+            newArray = newArray.filter(x => x !== e.target.value)
+        }
+        setData({...data, [e.target.name.slice(0, -2)]: newArray})
+        console.table(data.primaryPerksIDArray)
+    }
+    //works for checkboxes but no radios (picks too many)
+    function handleChangeRadioSecondary(e: ChangeEvent<HTMLInputElement>) {
+            //max  2 picked
+        let newArray = [...data.secondaryPerksIDArray, e.target.value];
+        // @ts-ignore never type
+        if(data.secondaryPerksIDArray.includes(e.target.value)){
+            newArray = newArray.filter(x => x !== e.target.value)
+        }
+        setData({...data, [e.target.name.slice(0, -2)]: newArray})
+        console.table(data.secondaryPerksIDArray)
+    }
     return (
         <>
             <div className="toast toast-top toast-start">
@@ -179,45 +130,70 @@ const bui: Prisma.BuildCreateInput = {
                     <span>{`${error.msg}`}</span>
                 </div>
             </div>
-            <form action={handleSubmit}>
-                <select name="champion">
+            <form onSubmit={handleSubmit}>
+                <select name="championID" onChange={e => handleChange(e)}>
                     {
                         champions?.map(champion => <option key={champion.id} value={champion.id}>{champion.name}</option>)
                     }
                 </select>
-                {/* <select name='keystone' onChange={e => handleChange(e)}>
 
-                    {runes.map(rune =>
-                        <option key={rune.id} value={rune.id}>{rune.name}</option>)}
+                <div className="join">
+                    {runes.sort((a, b) => a.league_id - b.league_id).map(rune =>
+                        <input key={rune.id} type="radio" className="join-item btn" name="primary_tree" aria-label={rune.name} value={rune.id} onChange={e => handleChange(e)} />)}
+                </div>
 
-                </select> */}
-                
+                {data.primary_tree ?
+                    <div className="join">
+                        {runes.filter(e => e.id != data.primary_tree).map(rune =>
+                            <input key={rune.id} type="radio" className="join-item btn" name="secondary_tree" aria-label={rune.name} value={rune.id} onChange={e => handleChange(e)} />)}
+                    </div>
+                    : null}
+
+
+
                 <div className="grid w-1/4">
 
                     {
-                        // [0, 1, 2, 3].map(e =>
-                        //     <div className='grid grid-flow-col'>
-                        //         {runes.find(e => e.id === picked?.keystone)
-                        //             ?.perks
-                        //             .map(perk => perk.row===e ? 
-                        //                 <>
-                        //                     <input type="radio" name={`perk_${perk.row}`} value={perk.id} onChange={e => handleChange(e)} />
-                        //                     <label htmlFor={perk.id}><Image height={64} width={64} src={`/images/runes/${runes.find(rune => rune.id === picked?.keystone)?.name}/${perk.key}/${perk.key}.png`} alt={perk.name} /> </label>
-                        //                 </> : null
-                        //             )
-                        //             }
-                        //     </div>
-                        // )
+                        [0, 1, 2, 3].map(e =>
+                            <div className='grid grid-flow-col' key={e.toString()}>
+                                {runes.find(e => e.id === data?.primary_tree)
+                                    ?.perks
+                                    .map(perk => perk.row === e ?
+                                        <>
+                                            <input type="radio" name={`primaryPerksIDArray_${perk.row}`} value={perk.id} onChange={e => handleChangeRadioPrimary(e)} />
+                                            <label htmlFor={perk.id}><Image height={64} width={64} src={`/images/runes/${runes.find(rune => rune.id === data.primary_tree)?.name}/${perk.key}/${perk.key}.png`} alt={perk.name} /> </label>
+                                        </> : null
+                                    )
+                                }
+                            </div>
+                        )
+
+                    }
+
+                </div>
+                <div className="grid w-1/4">
+                    {
+                        [1, 2, 3].map(e =>
+                            <div className='grid grid-flow-col' key={e.toString()}>
+                                {runes.find(e => e.id === data?.secondary_tree)
+                                    ?.perks
+                                    .map(perk => perk.row === e ?
+                                        <>
+                                            <input type="radio" name={`secondaryPerksIDArray_${perk.row}`} value={perk.id} onChange={e => handleChangeRadioSecondary(e)} />
+                                            <label htmlFor={perk.id}><Image height={64} width={64} src={`/images/runes/${runes.find(rune => rune.id === data.secondary_tree)?.name}/${perk.key}/${perk.key}.png`} alt={perk.name} /> </label>
+                                        </> : null
+                                    )
+                                }
+                            </div>
+                        )
 
                     }
                 </div>
 
-
-
                 <div className="grid grid-cols-12 w-1/2">
                     {items?.map(item =>
                         <div key={item.id}>
-                            <input type="checkbox" name="items" value={item.id} id={item.id} />
+                            <input type="checkbox" name="itemsIDArray" value={item.id} id={item.id} onChange={e => handleChangeCheckbox(e)} />
                             <label htmlFor={item.id}><Image height={64} width={64} src={`/images/item/${item.league_id}.png`} alt={item.name} /></label>
 
                         </div>
