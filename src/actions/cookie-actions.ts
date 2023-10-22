3f47fb84b7cbfe987b45e65529f2f6fd4e517bd6
Madvux <tom.wilkolazki@gmail.com>
2023-10-22 21:34:05 +0200

    set cookie to expire after 30 days

--- src/actions/cookie-actions.ts
+++ src/actions/cookie-actions.ts
@@ -2,7 +2,8 @@
 
 import { cookies } from 'next/headers'
 export const createCookie = async (data: FormData) => {
-    data.get('key') ? cookies().set('key', data.get('key') as string) : cookies().delete('key')
+    //cookie last month
+    data.get('key') ? cookies().set('key', data.get('key') as string, { maxAge: 60 * 60 * 24 * 30 }) : cookies().delete('key')
 }
 export const getCookie = async () => {
     return cookies().get('key')?.value ?? ""
